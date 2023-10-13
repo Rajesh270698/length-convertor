d85b866b99fb327a105ff77102881609db6aa286
Rajesh270698 <rajeshmurgesan18@gmail.com>
2023-10-13 11:51:26 +0530

    length con2

---
+++ script.js
@@ -0,0 +1,7 @@
+function convert() {
+
+    let cmVal = Number(document.getElementById("input").value)
+    let inVal = cmVal / 2.54
+    let result = document.getElementById("result")
+    result.innerHTML = inVal.toFixed(2) + "inches"
+}
