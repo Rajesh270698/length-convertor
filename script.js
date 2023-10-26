400ccb0da74b0f4f8eefddb7eff6ae6a26a3a801
Rajesh270698 <rajeshmurgesan18@gmail.com>
2023-10-26 21:33:34 +0530

    console

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function convert() {
 
     let cmVal = Number(document.getElementById("input").value)
     let inVal = cmVal / 2.54
+    console.log(inVal)
     let result = document.getElementById("result")
     result.innerHTML = inVal.toFixed(2) + "inches"
 }
